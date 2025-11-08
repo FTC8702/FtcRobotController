3e8b34a653e714f4594f591d5cda0896fe71be3f
ftc-taylorb-dev <ftc-taylorb@novalabsrobotics.org>
2025-11-07 20:44:18 -0500

    Made PintPoint_Execution work for reals this time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinPoint_Execution.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinPoint_Execution.java
@@ -9,18 +9,42 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.UnnormalizedAngleUnit;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 public class PinPoint_Execution {
     GoBildaPinpointDriver odo;
+    double lastXError = 0;
+    double lastYError = 0;
 
     // Constructor
     public PinPoint_Execution(GoBildaPinpointDriver odo){
         this.odo = odo;
     }
-    public void move(double x, double y, double z){
+    public List<Double> move(double x, double y, double z) {
         odo.setOffsets(-100, 0, DistanceUnit.MM);
         odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
         odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
-        double xError =
+        Pose2D pos = odo.getPosition();
+        double xError = pos.getX(DistanceUnit.MM) - x;
+        double yError = pos.getY(DistanceUnit.MM) - y;
+        double zError = pos.getHeading(AngleUnit.DEGREES) - z;
+        double xP = -0.25 * xError;
+        double yP = -0.25 * yError;
+        double xD = -0.25 * (xError - lastXError) / odo.getLoopTime();
+        double yD = -0.25 * (yError - lastYError) / odo.getLoopTime();
+
+        lastXError = xError;
+        lastYError = yError;
+
+        double linXVel = xP + xD;
+        double linYVel = yP + yD;
+        double fLVelocity = linXVel - linYVel;
+        double fRVelocity = linXVel + linYVel;
+        double rLVelocity = linXVel + linYVel;
+        double rRVelocity = linXVel - linYVel;
+        List<Double> velocity = Arrays.asList(fLVelocity, fRVelocity, rLVelocity, rRVelocity);
+        return velocity;
     }
 }
