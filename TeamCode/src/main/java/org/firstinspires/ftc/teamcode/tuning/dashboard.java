7a9283cd2c4780c3b05aaf5edda07f55c3277f7a
ftc-taylorb-dev <ftc-taylorb@novalabsrobotics.org>
2025-12-07 12:02:49 -0500

    Added roaddrunner framework

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/dashboard.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.tuning;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+public class dashboard {
+FtcDashboard dashboard = FtcDashboard.getInstance();
+
+}
+
